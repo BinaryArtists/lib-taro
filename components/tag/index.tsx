af5946d842204a2e57825a3fa253909072b4a8ca
fallen.ink <fengzilijie@qq.com>
2020-01-14 10:57:19 +0800

    tag css

--- components/tag/index.tsx
+++ components/tag/index.tsx
@@ -1,17 +1,13 @@
-import { View, Image, Text, Input, ScrollView, Swiper, SwiperItem } from '@tarojs/components'
+import { View, } from '@tarojs/components'
 import './index.scss';
 import { AtTag } from 'taro-ui'
 
 
 function Tag(props) {
   return (
-    <View style={{margin:'0 0 5px 5px',display:'inline-block'}}>
-
-      <AtTag  circle size='small'>{props.data}</AtTag>
-
+    <View style={{margin:'0 5px 0 5px',display:'inline-block',}}>
+      <AtTag circle size='small'>{props.data}</AtTag>
     </View>
-
-
   )
 }
 
